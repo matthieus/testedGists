61fc76f937fcc7a9242e181b320d356d4eb4d25d
mat <matthieu.scholler@gmail.com>
2013-01-19 17:47:47 +0000

    zip builder used

--- src/main/java/testedGists/TransformFileInZip.java
+++ src/main/java/testedGists/TransformFileInZip.java
@@ -23,137 +23,141 @@ import org.xml.sax.InputSource;
 import com.google.common.io.ByteStreams;
 
 /**
- * Tool to find and transform an arbitrary file in a zip file or nested zip files.
+ * Tool to find and transform an arbitrary file in a zip file or nested zip
+ * files.
  */
 public class TransformFileInZip {
 
-	/**
-	 * Allows defining the transformation to apply to the file being transformed
-	 */
-	public interface StreamTransformer {
-		void apply(InputStream input, OutputStream output);
-	}
+  /**
+   * Allows defining the transformation to apply to the file being transformed
+   */
+  public interface StreamTransformer {
+    void apply(InputStream input, OutputStream output);
+  }
 
-	/**
-	 * Wrapper around a ZipInputStream to avoid the xml parsing to close the stream
-	 */
-	public static class ZipGuard extends FilterInputStream {
-	    public ZipGuard(InputStream is) {
-	        super(is);
-	    }
-	    @Override
-	    public void close() throws IOException {
-	      if (in instanceof ZipInputStream)
-	        ((ZipInputStream) in).closeEntry();
-	      else
-	        in.close();
-	    }
-	}
+  /**
+   * Wrapper around a ZipInputStream to avoid the xml parsing to close the
+   * stream
+   */
+  public static class ZipGuard extends FilterInputStream {
+    public ZipGuard(InputStream is) {
+      super(is);
+    }
 
-	/**
-	 * How fileToTransformPath works:<br>
-	 * <ul>
-	 * <li>file directly in 1st level zip, "[directory/]file"</li>
-	 * <li>file contained in a zip inside zip, "[directory/]zipFile:[directory/]file"</li>
-	 * <li>you can add more level of zip containing zips, "zipLevel1:zipLevel2:...:zipLeveln:file"</li>
-	 * <li>full filename and directory is expected, no partial name search</li>
-	 * </ul>
-	 * fileToTransformPath sample: <code>uwb-war-4.7-SNAPSHOT.war:WEB-INF/web.xml</code>
-	 *
-	 * @param zipFile
-	 * @param targetZipFilePath the path of the new zip file to create
-	 * @param fileToTransformPath the path inside the zip (or multiple zips) to the file to transform
-	 * @param transformer the function object which will be applied to the target file
-	 */
-	public static void transformFile(ZipFile zipFile, String targetZipFilePath, String fileToTransformPath, StreamTransformer transformer) {
-		long start = System.currentTimeMillis();
-		System.out.println("Processing zip file \""+zipFile.getName()+"\".");
-		ZipInputStream zipInputStream = null;
-		ZipOutputStream zipOutputStream = null;
-		try {
-		zipInputStream = new ZipInputStream(new FileInputStream(zipFile.getName()));
-		zipOutputStream = new ZipOutputStream(new FileOutputStream(targetZipFilePath));
-		if (!transformZipStream(zipInputStream,
-						   zipOutputStream,
-						   fileToTransformPath,
-						   transformer)) {
-			System.err.println("The given file has not been found in the given zip file. No transformation applied.");
-		}
-		System.out.println("New zip file: \""+targetZipFilePath+"\"");
-		System.out.println("Execution time: "+(System.currentTimeMillis() - start)+"ms");
-		}catch(Exception e){throw new RuntimeException(e);}
-		finally {
-		try {
-		  zipInputStream.close();
-	      zipOutputStream.close();
-	    } catch (IOException e) {throw new RuntimeException(e);}
-		}
-	}
+    @Override
+    public void close() throws IOException {
+      if (in instanceof ZipInputStream)
+        ((ZipInputStream) in).closeEntry();
+      else
+        in.close();
+    }
+  }
 
-	/**
-	 * Recursive method which digs into the relevant war or modifies the target file.
-	 *
-	 * @param zipInputStream
-	 * @param zipOutputStream
-	 * @param fileToTransformPath the path to the file to transform (see transformFile() doc for details)
-	 * @param transformer
-	 * @return true if the file has been found and transformed, false otherwise
-	 */
-	static boolean transformZipStream(ZipInputStream zipInputStream, ZipOutputStream zipOutputStream, String fileToTransformPath, StreamTransformer transformer) {
-	  boolean applied = false;
-		try {
-		// TODO: handle escape of ':'
-		int colonIndex = fileToTransformPath.indexOf(':');
-		String currentFile = colonIndex == -1 ? fileToTransformPath : (String) fileToTransformPath.subSequence(0, colonIndex);
-		ZipEntry zipEntry = zipInputStream.getNextEntry();
-		while (zipEntry != null) {
-			String zipEntryName = zipEntry.getName();
-			zipOutputStream.putNextEntry(new ZipEntry(zipEntryName));
-			if (zipEntryName.equals(currentFile)) {
-				if (colonIndex == -1) { // we are looking for the file itself, no more zips to dig in
-					transformer.apply(new ZipGuard(zipInputStream), zipOutputStream);
-					applied = true;
-				} else { 			    // we are digging into more zip files
-					applied = transformZipStream(new ZipInputStream(zipInputStream),
-									   new ZipOutputStream(zipOutputStream),
-									   fileToTransformPath.substring(colonIndex+1),
-									   transformer);
-				}
-			} else {
-				if (!zipEntry.isDirectory())
-					ByteStreams.copy(zipInputStream, zipOutputStream);
-			}
-			zipOutputStream.closeEntry();
-			zipEntry = zipInputStream.getNextEntry();
-		}
-		} catch (Exception e) {throw new RuntimeException(e);}
-		finally {
-		try {
-      zipOutputStream.finish();
-      zipInputStream.closeEntry();
-    } catch (IOException e) { throw new RuntimeException(e);}
-		}
-		return applied;
-	}
+  /**
+   * How fileToTransformPath works:<br>
+   * <ul>
+   * <li>when parsing the given path, zip file names are considered as
+   * directories
+   * <li>file contained in a zip inside zip,
+   * "[directory/]zipFile/[directory/]file"</li>
+   * <li>you can add more levels of zip containing zips,
+   * "zipLevel1/zipLevel2/.../zipLeveln/file"</li>
+   * <li>full filename and directory is expected, no partial name search</li>
+   * </ul>
+   * fileToTransformPath sample:
+   * <code>uwb-war-4.7-SNAPSHOT.war/WEB-INF/web.xml</code>
+   *
+   * @param zipFile
+   * @param targetZipFilePath
+   *          the path of the new zip file to create
+   * @param fileToTransformPath
+   *          the path inside the zip (or multiple zips) to the file to
+   *          transform, cannot be a directory or a zip file
+   * @param transformer
+   *          the function object which will be applied to the target file
+   */
+  public static void transformFile(ZipFile zipFile, String targetZipFilePath,
+      String fileToTransformPath, StreamTransformer transformer) {
+    long start = System.currentTimeMillis();
+    System.out.println("Processing zip file \"" + zipFile.getName() + "\".");
+    ZipInputStream zipInputStream = null;
+    ZipOutputStream zipOutputStream = null;
+    try {
+      zipInputStream = new ZipInputStream(
+          new FileInputStream(zipFile.getName()));
+      zipOutputStream = new ZipOutputStream(new FileOutputStream(
+          targetZipFilePath));
+      if (!transformZipStream(zipInputStream, zipOutputStream,
+          fileToTransformPath, transformer)) {
+        System.err
+            .println("The given file has not been found in the given zip file. No transformation applied.");
+      }
+      System.out.println("New zip file: \"" + targetZipFilePath + "\"");
+      System.out.println("Execution time: "
+          + (System.currentTimeMillis() - start) + "ms");
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      try {
+        zipInputStream.close();
+        zipOutputStream.close();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
 
-	public static void main(String[] args) throws IOException {
-		// example of usage:
-		// TODO: modify with sample file or remove once the test is written
-		transformFile(new ZipFile("/work/ure-base/ure-parent/ure/ure-ear/target/ure-ear-4.7-SNAPSHOT.ear"),
-					  "/work/ure-base/ure-parent/ure/ure-ear/ure-ear-4.7-++SNAPSHOT-nosecurity.ear",
-					  "uwb-war-4.7-SNAPSHOT.war:WEB-INF/web.xml",
-					  new StreamTransformer() {
-						@Override
-						public void apply(InputStream input, OutputStream output) {
-							try {
-							Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-												.parse(new InputSource(input));
-							Node toRemove = doc.getElementsByTagName("security-constraint").item(0);
-							doc.getDocumentElement().removeChild(toRemove);
-							TransformerFactory.newInstance().newTransformer()
-								.transform(new DOMSource(doc), new StreamResult(output));
-							} catch(Exception e) {throw new RuntimeException(e);}
-						}
-					});
-	}
+  /**
+   * Recursive method which digs into the relevant war or modifies the target
+   * file.
+   *
+   * @param zipInputStream
+   * @param zipOutputStream
+   * @param fileToTransformPath
+   *          the path to the file to transform (see transformFile() doc for
+   *          details)
+   * @param transformer
+   * @return true if the file has been found and transformed, false otherwise
+   */
+  static boolean transformZipStream(ZipInputStream zipInputStream,
+      ZipOutputStream zipOutputStream, String fileToTransformPath,
+      StreamTransformer transformer) {
+    boolean applied = false;
+    try {
+      ZipEntry zipEntry = zipInputStream.getNextEntry();
+      while (zipEntry != null) {
+        String zipEntryName = zipEntry.getName();
+        zipOutputStream.putNextEntry(new ZipEntry(zipEntryName));
+        if (fileToTransformPath.startsWith(zipEntryName)) {
+          if (fileToTransformPath.equals(zipEntryName)) { // we are looking for
+                                                          // the file itself, no
+                                                          // more zips to dig in
+            transformer.apply(new ZipGuard(zipInputStream), zipOutputStream);
+            applied = true;
+          } else { // we are digging into more zip files
+            applied = transformZipStream(new ZipInputStream(zipInputStream),
+                new ZipOutputStream(zipOutputStream),
+                fileToTransformPath.substring(zipEntryName.length() + 1),
+                transformer);
+          }
+        } else {
+          if (!zipEntry.isDirectory())
+            ByteStreams.copy(zipInputStream, zipOutputStream);
+        }
+        zipOutputStream.closeEntry();
+        zipInputStream.closeEntry();
+        zipEntry = zipInputStream.getNextEntry();
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      try {
+        zipOutputStream.finish();
+        zipInputStream.closeEntry();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return applied;
+  }
 }
--- src/test/scala/testedGists/TransformFileInZipSpec.scala
+++ src/test/scala/testedGists/TransformFileInZipSpec.scala
@@ -1,100 +1,180 @@
 package testedGists
 
 import org.specs2._
+import specification.Step
 import java.io._
 import java.util.zip.ZipOutputStream
 import testedGists.TransformFileInZip.StreamTransformer
 import testedGists.TransformFileInZip.ZipGuard
 import java.util.Scanner
-import java.util.zip.ZipInputStream
-import java.util.zip.ZipFile
-import java.util.zip.ZipEntry
+import java.util.zip._
 
-class TransformFileInZipSpec extends Specification { def is =
+class TransformFileInZipSpec extends Specification {
+  def is =
 
-	"TransformFileInZipSpec should" ^
-		"remove 'TO_DELETE' from file 'TO_BE_FOUND.txt' in root directory of zip file 'myZip.zip'" ! new zipFixture().e1
+    "TransformFileInZipSpec should"                                                          ^
+      "remove 'TO_DELETE' from file 'TO_BE_FOUND.txt' in root directory of zip file"         ! context().e1 ^
+      "remove 'TO_DELETE_2' from file 'TO_BE_FOUND_2.txt' in zip file contained in zip file" ! context().e2 ^
+                                                                                             Step(deleteZipFixture) ^
+                                                                                             end
 
-	case class zipFixture() {
-		import java.util.zip._
-		val tempFileFixture = File.createTempFile("fixture", "")
-		val tempFileResult = File.createTempFile("result", "")
-		val zipFixture = {
-			safeUse(new ZipOutputStream(new FileOutputStream(tempFileFixture)))(_.close) { zos =>
-				zos.putNextEntry(new ZipEntry("TO_BE_FOUND.txt"))
-				printToStream(zos){_.print("blob TO_DELETE blob")}
-			}
-			new ZipFile(tempFileFixture)
-		}
+  val zipFixture =
+    safeZipBuilder(File.createTempFile("fixture", "")) {
+        _
+        .startEntry("TO_BE_FOUND.txt")
+          .print("blob TO_DELETE blob")
+        .endEntry
+        .startEntry("someFile.txt")
+          .print("blob?")
+        .endEntry
+        .startZipEntry("ZIP_TO_BE_FOUND.zip")
+          .startEntry("TO_BE_FOUND_2.txt")
+            .print("""|first line
+                      |second TO_DELETE_2 line""".stripMargin)
+          .endEntry
+        .endEntry
+        .buildZip
+      }
+  def deleteZipFixture {
+    new File(zipFixture.getName).delete
+  }
 
-		def e1 = {
-		    TransformFileInZip.transformFile(zipFixture, tempFileResult.getAbsolutePath(), "TO_BE_FOUND.txt",
-		        new StreamTransformer(){
-		          override def apply(input: InputStream, output: OutputStream) {
-		          	transformLines(input, output) { line =>
-		          		line.replaceAll("TO_DELETE", "")
-		          	}
-		          }
-		    })
-		    contentAsString(new ZipFile(tempFileResult), "TO_BE_FOUND.txt") must_== "blob  blob"
-		}
-	}
+  case class context() extends specification.After{
+    val tempFileResult = File.createTempFile("result", "")
+    override def after {tempFileResult.delete}
 
-	def transformLines(input: InputStream, os: OutputStream)(op: String => String) {
-		val scanner = new Scanner(new ZipGuard(input))
-		safeUse(os)(_ match {case outZip: ZipOutputStream => outZip.closeEntry()}){os =>
-	  		safeUse(scanner)(_.close) {s=>
-	  			while(s.hasNextLine) {
-	  				os.write(op(s.nextLine).getBytes)
-	  			}
-	  		}
-		}
-	}
+    def e1 = this {
+      TransformFileInZip.transformFile(zipFixture, tempFileResult.getAbsolutePath(), "TO_BE_FOUND.txt",
+        new StreamTransformer() {
+          override def apply(input: InputStream, output: OutputStream) {
+            mapContent(input, output) { content =>
+              content.replaceAll("TO_DELETE", "")
+            }
+          }
+        })
+      contentAsString(new ZipFile(tempFileResult), "TO_BE_FOUND.txt") must_== "blob  blob"
+    }
 
-	def contentAsString(zipFile: ZipFile, targetFileZipPath: String): String = {
-		val zis = new ZipInputStream(new FileInputStream(zipFile.getName()))
-		def findFileInZip(zis: ZipInputStream, targetFileZipPath: String) {
-			val zipEntry = zis.getNextEntry
-			if (zipEntry == null)
-				throw new FileNotFoundException
-			// TODO: handle escape of ':'
-			val colonIndex = targetFileZipPath.indexOf(':')
-			val currentFileName = if (colonIndex == -1) targetFileZipPath else targetFileZipPath.take(colonIndex)
-			if (zipEntry.getName != currentFileName) {
-				findFileInZip(zis, targetFileZipPath)
-			} else {
-				if (colonIndex != -1)
-					findFileInZip(zis, targetFileZipPath.drop(colonIndex+2))
-				// else we found it as the currentFileName
-			}
-		}
-		findFileInZip(zis, targetFileZipPath)
-		val scanner = new Scanner(zis).useDelimiter("\\A")
-		if (scanner.hasNext) scanner.next else ""
-	}
-
-	def printToStream(os: OutputStream)(op: java.io.PrintWriter => Unit) {
-		val p = new java.io.PrintWriter(os)
-		try {op(p)}
-		finally {p.flush}
-	}
-
-  def safeUse[A](resource: A)(cleanUp: A => Unit)(code: A => Unit) = {
-    try {
-      try { code(resource) }
-      finally { cleanUp(resource) }
-    } catch {
-      case e: Exception => throw new RuntimeException(e)
+    def e2 = this {
+      TransformFileInZip.transformFile(zipFixture, tempFileResult.getAbsolutePath(), "ZIP_TO_BE_FOUND.zip/TO_BE_FOUND_2.txt",
+        new StreamTransformer() {
+          override def apply(input: InputStream, output: OutputStream) {
+            mapContent(input, output) { content =>
+              content.replaceAll("TO_DELETE_2", "")
+            }
+          }
+        })
+      val content = contentAsString(new ZipFile(tempFileResult), "ZIP_TO_BE_FOUND.zip/TO_BE_FOUND_2.txt")
+      content.lines.drop(1).next must_== "second  line"
     }
   }
 
-// TODO:
-	case class ZipBuilder(path: String) {
+  def mapContent(is: InputStream, os: OutputStream)(op: String => String) {
+    val writer = new PrintWriter(os)
+    try {
+      writer.print(op(stringFromInputStream(is)))
+    } finally {
+      writer.flush
+    }
+  }
 
-		val zos = new ZipOutputStream(new FileOutputStream(path))
+  def contentAsString(zipFile: ZipFile, targetFileZipPath: String): String = {
+    val zis = new ZipInputStream(new FileInputStream(zipFile.getName()))
+    def contentOfFileInZip(zis: ZipInputStream, targetFileZipPath: String): String = {
+      val zipEntry = zis.getNextEntry
+      if (zipEntry == null)
+        throw new FileNotFoundException("path: "+targetFileZipPath)
+      if (!targetFileZipPath.startsWith(zipEntry.getName)) {
+        contentOfFileInZip(zis, targetFileZipPath)
+      } else {
+        if (!targetFileZipPath.equals(zipEntry.getName))
+          contentOfFileInZip(new ZipInputStream(zis), targetFileZipPath.drop(zipEntry.getName.length + 1))
+        else {
+          stringFromInputStream(zis)
+        }
+      }
+    }
+    safeClose(zis){ zis =>
+      contentOfFileInZip(zis, targetFileZipPath)
+    }
+  }
 
-		def startEntry(path: String)() {
-			zos.putNextEntry(new ZipEntry(path))
-		}
-	}
+  def stringFromInputStream(is: InputStream): String = {
+    val scanner = new Scanner(is).useDelimiter("\\A")
+    if (scanner.hasNext) scanner.next else ""
+  }
+
+  def printToStream(os: OutputStream)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(os)
+    try { op(p) }
+    finally { p.flush }
+  }
+
+  def safeUse[A,B](resource: A)(cleanUp: A => Unit)(code: A => B): B = {
+    try {
+      try { code(resource) }
+      finally {
+        try {
+          cleanUp(resource)
+          } catch {
+            case e: Exception => throw new RuntimeException("Cleanup failed: " + e.printStackTrace)
+          }
+      }
+    } catch {
+      case e: Exception => throw new RuntimeException("Code application failed: " + e.printStackTrace)
+    }
+  }
+
+  def safeClose[A <: Closeable, B](resource: A)(code: A => B): B = {
+    safeUse(resource)(_.close)(code)
+  }
+
+  def safeZipBuilder(file: File)(op: RootZipBuilder => Unit): ZipFile = {
+    safeClose(new ZipOutputStream(new FileOutputStream(file))) { zos =>
+      op(RootZipBuilder(zos))
+    }
+    new ZipFile(file)
+  }
+  case class EntryBuilder(parent: ZipBuilder, zos: ZipOutputStream) {
+    def print(l: String): EntryBuilder = {
+      printToStream(zos)(_.print(l))
+      this
+    }
+    def endEntry(): ZipBuilder = {
+      zos.closeEntry
+      parent
+    }
+  }
+  trait ZipBuilder {
+    val zos: ZipOutputStream
+    def startEntry(path: String): EntryBuilder = {
+      zos.putNextEntry(new ZipEntry(path))
+      EntryBuilder(this, zos)
+    }
+    def startZipEntry(path: String): ZipEntryBuilder = {
+      zos.putNextEntry(new ZipEntry(path))
+      ZipEntryBuilder(this, new ZipOutputStream(zos))
+    }
+    def endEntry(): ZipBuilder
+    def buildZip()
+  }
+  case class ZipEntryBuilder(parent: ZipBuilder, zos: ZipOutputStream) extends ZipBuilder {
+    override def endEntry(): ZipBuilder = {
+      zos.closeEntry
+      zos.finish
+      parent
+    }
+    override def buildZip() {
+      throw new UnsupportedOperationException
+    }
+  }
+  case class RootZipBuilder(zos: ZipOutputStream) extends ZipBuilder{
+    override def endEntry() = {
+      throw new UnsupportedOperationException
+    }
+    override def buildZip() {
+      zos.finish
+      zos.close
+    }
+  }
 }
